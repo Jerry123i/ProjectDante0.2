3a9774ca72e557b83473727e0346fc76d2a65946
Jerry123i <igorsc.jerry@gmail.com>
2017-05-17 19:14:21 -0300

    Enviando arquivios organizados

---
+++ Assets/Scripts/SceneScripts/SpawnerScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerScript : MonoBehaviour
+{
+
+    public GameObject[] spawners;
+    public GameObject enemy;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        spawners = GameObject.FindGameObjectsWithTag("Spawner");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("p"))
+        {
+            SpawnNEnemies(4, enemy);
+        }
+    }
+
+    void SpawnNEnemies(int n, GameObject enemyPrefab)
+    {
+
+        for (int i = 0; i < n; i++)
+        {
+            GameObject enemy = Instantiate(enemyPrefab, spawners[Random.Range(0, spawners.Length)].transform.position, Quaternion.Euler(0,0,0));
+        }
+
+    }
+        
+}
