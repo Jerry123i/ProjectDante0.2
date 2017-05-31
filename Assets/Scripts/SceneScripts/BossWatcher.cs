36a123f0f6d65d2cff2a7ad0dd749633967baa28
Ruan da Fonseca Ramos <ruan.ramos28@gmail.com>
2017-05-31 18:36:50 -0300

    Boss arrumado

--- Assets/Scripts/SceneScripts/BossWatcher.cs
+++ Assets/Scripts/SceneScripts/BossWatcher.cs
@@ -7,10 +7,18 @@ public class BossWatcher : MonoBehaviour {
 
     public GameObject theBoss;
     public GameObject thePlayer;
-    
+    public GameObject theProjectile;
 
-	// Use this for initialization
-	void Start () {
+
+    private void Awake()
+    {
+        theProjectile.GetComponent<projectileScript>().speed = 0;
+        
+        thePlayer.GetComponent<CharacterActions>().spearOn = false;
+    }
+
+    // Use this for initialization
+    void Start () {
 
 
     }
@@ -18,7 +26,12 @@ public class BossWatcher : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-        
+        thePlayer.GetComponent<CharacterActions>().SetSprite();
+        if (theProjectile.transform.position == new Vector3(0, 0, 0) && theProjectile.GetComponent<projectileScript>().speed == 0 && theProjectile.transform.rotation.x == 0 && theProjectile.transform.rotation.y == 0 && theProjectile.transform.rotation.z == 0)
+        {
+            theProjectile.GetComponent<projectileScript>().isProjectile = false;
+        }
+
         if (theBoss.gameObject == null)
         {
             SceneManager.LoadScene("WinScene");
